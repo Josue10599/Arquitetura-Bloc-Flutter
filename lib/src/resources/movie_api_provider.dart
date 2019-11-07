967dc37e34c7fedcabdda1f42967eaec480a1452
Josue <josue10599@gmail.com>
2019-11-07 15:42:23 -0300

    Corrigida a apresentação dos filmes

--- lib/src/resources/movie_api_provider.dart
+++ lib/src/resources/movie_api_provider.dart
@@ -2,16 +2,17 @@ import 'dart:convert';
 
 import 'package:arquitetura_bloc/api.dart';
 import 'package:arquitetura_bloc/src/models/item_model.dart';
-import 'package:http/http.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' show Client;
 
 class MovieApiProvider {
   Client client = Client();
   final _apiKey = apiKey;
 
   Future<ItemModel> fetchMovieList() async {
-    print('entered');
+    debugPrint('entered');
     final response = await client.get('http://api.themoviedb.org/3/movie/popular?api_key=$_apiKey');
-    print(response.body.toString());
+    debugPrint(response.body.toString());
     if (response.statusCode == 200) {
       return ItemModel.fromJson(json.decode(response.body));
     } else {
--- lib/src/ui/movie_list.dart
+++ lib/src/ui/movie_list.dart
@@ -1,32 +1,73 @@
+import 'package:arquitetura_bloc/src/blocs/movies_bloc.dart';
 import 'package:arquitetura_bloc/src/models/item_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class MovieList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    bloc.fetchAllMovies();
     return Scaffold(
       appBar: AppBar(
         title: Text("Filmes Populares"),
       ),
-      body: StreamBuilder(builder: (context, AsyncSnapshot<ItemModel> snapshot) {
-        if (snapshot.hasData)
-          return buildList(snapshot);
-        else if (snapshot.hasError) return Text(snapshot.error.toString());
-        return Center(
-          child: CircularProgressIndicator(),
-        );
-      }),
+      body: StreamBuilder(
+        stream: bloc.allMovies,
+        builder: (context, AsyncSnapshot<ItemModel> snapshot) {
+          if (snapshot.hasData)
+            return buildList(snapshot);
+          else if (snapshot.hasError) return Text(snapshot.error.toString());
+          return Center(child: CircularProgressIndicator());
+        },
+      ),
     );
   }
 
   Widget buildList(AsyncSnapshot<ItemModel> snapshot) {
     return GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-        itemBuilder: (BuildContext context, int index) {
-          return Image.network(
-            'https://image.tmdb.org/t/p/w185${snapshot.data.results[index].posterPath}',
-            fit: BoxFit.cover,
-          );
-        });
+      itemCount: snapshot.data.results.length,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+      ),
+      itemBuilder: (BuildContext context, int index) {
+        return _cardOfMovie(snapshot.data.results[index].title,
+            'https://image.tmdb.org/t/p/w185${snapshot.data.results[index].posterPath}');
+      },
+    );
   }
+
+  Widget _cardOfMovie(String name, String urlImage) =>
+      Card(
+        elevation: 2,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            _imageMovie(urlImage),
+            _nameMovie(name),
+          ],
+        ),
+      );
+
+  Widget _nameMovie(String name) =>
+      Container(
+        margin: EdgeInsets.all(4.0),
+        child: Text(
+          name,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 14.0,
+          ),
+        ),
+      );
+
+  Widget _imageMovie(String url) =>
+      AspectRatio(
+        aspectRatio: 3 / 2,
+        child: Image.network(
+          url,
+          fit: BoxFit.fitHeight,
+        ),
+      );
 }
